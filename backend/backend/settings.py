6cdf9d5d3a96c0a6c0fd533eda34262e9b47b5ae
Your Name <you@example.com>
2025-04-27 20:25:13 +0300

    add settings ,

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework'
+    'rest_framework',
     'corsheaders',
     'drf_yasg',
     'application',
